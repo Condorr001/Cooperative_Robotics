d822d4109c6c64c37e4cf74bb3b8f976f0b5a4a9
danielerialdi <72574090+danielerialdi@users.noreply.github.com>
2024-12-13 16:59:22 +0100

    Plots update

--- Exercise 2/PrintPlot.m
+++ Exercise 2/PrintPlot.m
@@ -140,33 +140,33 @@ legend('qdot_1','qdot_2','qdot_3','qdot_4','qdot_5','qdot_6','qdot_7');
 %% Activation functions
 % Left arm
 fig = figure("Name","Left Arm Activation Function");
-plot(plt.t(1:end), plt.armL.a_ma, plt.t(1:end), plt.armL.a_tool(1,:), plt.t(1:end), plt.a_rc(1,:), "LineWidth",2);
+plot(plt.t(1:end), plt.armL.a_ma, plt.t(1:end), plt.armL.a_tool(1,:), plt.t(1:end), plt.a_rc(1,:), plt.t(1:end), plt.armL.a_jl(1,:), "LineWidth",2);
 grid on;
 xlabel("Time [s]");
 ylabel("Activation");
 %fontsize(gca,22,"points");
 title("Left Arm Activation Function");
 %lgd = legend("MA","T","RC","Location","northeastoutside");
-lgd = legend("MA","T","RC");
+lgd = legend("MA","T","RC", "JL");
 %fontsize(lgd,22,"points");
 ylim([-0.1 1.1]);
 
 % Right arm
 fig = figure("Name","Right Arm Activation Function");
-plot(plt.t(1:end), plt.armR.a_ma, plt.t(1:end), plt.armR.a_tool(1,:), plt.t(1:end), plt.a_rc(1,:), "LineWidth",2);
+plot(plt.t(1:end), plt.armR.a_ma, plt.t(1:end), plt.armR.a_tool(1,:), plt.t(1:end), plt.a_rc(1,:), plt.t(1:end), plt.armR.a_jl(1,:), "LineWidth",2);
 grid on;
 xlabel("Time [s]");
 ylabel("Activation");
 %fontsize(gca,22,"points");
 title("Right Arm Activation Function");
 %lgd = legend("MA","T","RC","Location","northeastoutside");
-lgd = legend("MA","T","RC");
+lgd = legend("MA","T","RC", "JL");
 %fontsize(lgd,22,"points");
 ylim([-0.1 1.1]);
 
 %% Desired vs end-effector angular velocities
 % Left arm
-fig = figure("Name", "Left Arm Desired vs End-Effector Angular Velocities");
+fig = figure("Name", "Left Arm End-Effector Desired vs Actual Angular Velocities");
 plot(plt.t(1:end), plt.armL.xdot_tool(1:3, :),"LineWidth", 2);
 hold on;
 plot(plt.t(1:end), plt.armL.xdot(1:3, :) ,"LineWidth", 2);
@@ -174,12 +174,12 @@ grid on;
 xlabel("Time [s]");
 ylabel("Velocities [rad/s]");
 %fontsize(gca,22,"points");
-title("Left Arm Desired vs End-Effector Angular Velocities");
-lgd = legend("w_x_{des}","w_y_{des}","w_z_{des}","w_x_{ee}","w_y_{ee}","w_z_{ee}");
+title("Left Arm End-Effector Desired vs Actual Angular Velocities");
+lgd = legend("w_x_des","w_y_des","w_z_des","w_x_ee","w_y_ee","w_z_ee");
 %fontsize(lgd,22,"points");
 
 % Right arm
-fig = figure("Name", "Right Arm Desired vs End-Effector Angular Velocities");
+fig = figure("Name", "Right Arm End-Effector Desired vs Actual Angular Velocities");
 plot(plt.t(1:end), plt.armR.xdot_tool(1:3, :),"LineWidth", 2);
 hold on;
 plot(plt.t(1:end), plt.armR.xdot(1:3, :) ,"LineWidth", 2);
@@ -187,13 +187,13 @@ grid on;
 xlabel("Time [s]");
 ylabel("Velocities [rad/s]");
 %fontsize(gca,22,"points");
-title("Right Arm Desired vs End-Effector Angular Velocities");
-lgd = legend("w_x_{des}","w_y_{des}","w_z_{des}","w_x_{ee}","w_y_{ee}","w_z_{ee}");
+title("Right Arm End-Effector Desired vs Actual Angular Velocities");
+lgd = legend("w_x_des","w_y_des","w_z_des","w_x_ee","w_y_ee","w_z_ee");
 %fontsize(lgd,22,"points");
 
 %% Desired vs end-effector linear velocities
 % Left arm
-fig = figure("Name", "Left Arm Desired vs End-Effector Linear Velocities");
+fig = figure("Name", "Left Arm End-Effector Desired vs Actual Linear Velocities");
 plot(plt.t(1:end), plt.armL.xdot_tool(4:6, :),"LineWidth", 2);
 hold on;
 plot(plt.t(1:end), plt.armL.xdot(4:6, :) ,"LineWidth", 2);
@@ -201,12 +201,12 @@ grid on;
 xlabel("Time [s]");
 ylabel("Velocities [m/s]");
 %fontsize(gca,22,"points");
-title("Left Arm Desired vs End-Effector Linear Velocities");
-lgd = legend("v_x_{des}","v_y_{des}","v_z_{des}","v_x_{ee}","v_y_{ee}","v_z_{ee}");
+title("Left Arm End-Effector Desired vs Actual Linear Velocities");
+lgd = legend("v_x_des","v_y_des","v_z_des","v_x_ee","v_y_ee","v_z_ee");
 %fontsize(lgd,22,"points");
 
 % Right arm
-fig = figure("Name", "Right Arm Desired vs End-Effector Linear Velocities");
+fig = figure("Name", "Right Arm End-Effector Desired vs Actual Linear Velocities");
 plot(plt.t(1:end), plt.armR.xdot_tool(4:6, :),"LineWidth", 2);
 hold on;
 plot(plt.t(1:end), plt.armR.xdot(4:6, :) ,"LineWidth", 2);
@@ -214,8 +214,8 @@ grid on;
 xlabel("Time [s]");
 ylabel("Velocities [m/s]");
 %fontsize(gca,22,"points");
-title("Right Arm Desired vs End-Effector Linear Velocities");
-lgd = legend("v_x_{des}","v_y_{des}","v_z_{des}","v_x_{ee}","v_y_{ee}","v_z_{ee}");
+title("Right Arm End-Effector Desired vs Actual Linear Velocities");
+lgd = legend("v_x_des","v_y_des","v_z_des","v_x_ee","v_y_ee","v_z_ee");
 %fontsize(lgd,22,"points");
 
 
